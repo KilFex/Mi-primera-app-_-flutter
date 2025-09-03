9af925fd0f8e9b19b4b6b66ba8be16f6e92fa5a6
KilFex <elivan.1251996@gmail.com>
2025-09-02 22:35:09 -0500

    mi primera app flutter

---
+++ lib/layouts/column.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColumnExample extends StatelessWidget {
+  const ColumnExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa"),
+        Text("hola Lauraaaaa")
+        
+      ],
+    );
+  }
+}
